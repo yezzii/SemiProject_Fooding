1f1779a3e819d01534ff784e82b3bd8f82b19bb3
OkayDonkey <122771632+OkayDonkey@users.noreply.github.com>
2023-04-17 09:08:02 +0900

    Update FrontController.java

--- Semi_Project/src/main/java/com/member/controller/FrontController.java
+++ Semi_Project/src/main/java/com/member/controller/FrontController.java
@@ -36,7 +36,7 @@ public class FrontController extends HttpServlet {
 		ActionForward forward = null;
 		Properties prop = new Properties();
 		FileInputStream fis = new FileInputStream(
-				"C:\\Users\\Home\\Documents\\GitHub\\Semi_Project\\src\\main\\java\\com\\member\\controller\\mapping.properties");
+				"C:\\Users\\DongHyun\\Documents\\GitHub\\Semi_project\\Semi_Project\\src\\main\\java\\com\\member\\controller\\mapping.properties");
 
 		prop.load(fis);
 
