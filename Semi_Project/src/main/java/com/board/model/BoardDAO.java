c63f845dca5616c771a91455234b136759918ea8
Grumvle <121842291+Grumvle@users.noreply.github.com>
2023-04-18 09:25:40 +0900

    123

--- Semi_Project/src/main/java/com/board/model/BoardDAO.java
+++ Semi_Project/src/main/java/com/board/model/BoardDAO.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class BoardDAO {
 
 	Connection con = null;
--- Semi_Project/src/main/java/com/member/controller/FrontController.java
+++ Semi_Project/src/main/java/com/member/controller/FrontController.java
@@ -36,7 +36,7 @@ public class FrontController extends HttpServlet {
 		ActionForward forward = null;
 		Properties prop = new Properties();
 		FileInputStream fis = new FileInputStream(
-				"C:\\Users\\ll001\\OneDrive\\문서\\GitHub\\Semi_project\\Semi_Project\\src\\main\\java\\com\\member\\controller\\mapping.properties");
+				"C:\\Users\\ll001\\Documents\\GitHub\\Semi_project\\Semi_Project\\src\\main\\java\\com\\member\\controller\\mapping.properties");
 
 
 
